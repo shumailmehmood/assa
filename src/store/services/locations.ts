caf6ee9ac92966d9e7922615dd2e663006112103
shumailmehmood <shumailmehmood1@gmail.com>
2023-06-13 13:39:12 +0500

    Adding Progressbar

--- src/store/services/locations.ts
+++ src/store/services/locations.ts
@@ -1,6 +1,4 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
-
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: 'https://rickandmortyapi.com' }),
     tagTypes: ['Post'],
--- src/views/home/App.tsx
+++ src/views/home/App.tsx
@@ -1,51 +1,62 @@
 import React, { ReactElement, useState } from "react";
 import Card from "../../components/home/Card";
-import "../../assets/css/home/App.scss"
+import "../../assets/css/home/App.scss";
 import Paginating from "../../components/home/Paginator";
-import { useGetLocationsQuery } from "../../store/services/locations"
-import Skeleton from '@mui/material/Skeleton';
-// import history from "../../utils/history";
-import { useHistory, useLocation } from 'react-router-dom'
-
-
+import { useGetLocationsQuery } from "../../store/services/locations";
+import Skeleton from "@mui/material/Skeleton";
 
 const Home: React.FC = (): ReactElement => {
-  const [currentPage, setCurrentPage] = useState<any>(1)
-  const history = useHistory();
-  const locate = useLocation();
+  const [currentPage, setCurrentPage] = useState<any>(1);
   const {
-    data,
+    data: locations,
     isLoading,
     // isSuccess,
     // isError,
     // error,
-  } = useGetLocationsQuery(currentPage)
+  } = useGetLocationsQuery(currentPage);
 
-  function onChangeHandler(event: React.ChangeEvent<unknown>, page: number): void {
-    setCurrentPage(page)
+  function onChangeHandler(
+    event: React.ChangeEvent<unknown>,
+    page: number
+  ): void {
+    setCurrentPage(page);
   }
 
   function onRouteChange() {
-    locate('/location')
+    // locate("/location");
   }
 
   if (isLoading) {
-    return <div className="card-section">
-      {new Array(20).fill(0).map(() => <Skeleton variant="rectangular" width={420} height={130} />)}
-    </div>
+    return (
+      <div className="card-section">
+        {new Array(20).fill(0).map(() => (
+          <Skeleton variant="rectangular" width={420} height={130} />
+        ))}
+      </div>
+    );
   }
-  return <>
-    <section className="card-section">
-      <div className="cards">
-        {data && data.results.map((row: any) => (<div className="card-align" onClick={onRouteChange}
-          key={row.id}><Card data={row} /></div>))}
-      </div>
-      <div className="pagination">
-        {data && <Paginating count={Math.ceil(data.info.count / 20)} onChangeHandler={onChangeHandler} />}
-      </div>
-    </section>
-
-  </>;
+  return (
+    <>
+      <section className="card-section">
+        <div className="cards">
+          {locations &&
+            locations.results.map((row: any) => (
+              <div className="card-align" key={row.id}>
+                <Card data={row} />
+              </div>
+            ))}
+        </div>
+        <div className="pagination">
+          {locations && (
+            <Paginating
+              count={Math.ceil(locations.info.count / 20)}
+              onChangeHandler={onChangeHandler}
+            />
+          )}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;

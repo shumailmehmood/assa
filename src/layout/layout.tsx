6ebefed1a867d4f67a5bd8a12960388cf70b6387
= <=>
2023-06-14 21:37:07 +0500

    setup gh-pages

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,11 +1,14 @@
 import { ReactNode, ReactElement } from "react";
-import NavBar from "../components/layout/Nav"
+import NavBar from "../components/layout/Nav";
+
 
 interface Props {
     children: ReactNode
 }
 
 const Layout: React.FC<Props> = ({ children }): ReactElement => {
+
+    // console.log('Layout', location, isHomeRoute)
     return (
         <>
             <NavBar />
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,25 +4,28 @@ import Layout from "../layout/layout";
 import history from "../utils/history";
 import LinearProgress from "@mui/material/LinearProgress";
 
+
 const Home = lazy(() => import("../views/home/App"));
 const Location = lazy(() => import("../views/location/Location"));
 const Detail = lazy(() => import("../views/detail/Detail"));
 
 const IndexRouter: React.FC = (): ReactElement => {
-  return (
-    <Layout>
-      <BrowserRouter>
 
+  console.log('Router')
+  return (
+
+    <BrowserRouter>
+      <Layout>
         <Suspense fallback={<LinearProgress />}>
-          <Routes>
+          <Routes >
             <Route path="/" element={<Home />} />
             <Route path="/location" element={<Location />} />
             <Route path="/detail" element={<Detail />} />
           </Routes>
         </Suspense>
+      </Layout>
+    </BrowserRouter>
 
-      </BrowserRouter>
-    </Layout>
   );
 };
 

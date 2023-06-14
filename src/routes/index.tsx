7a66ea413c40c77bb22a4f1ee9fbf466ad5996e0
= <=>
2023-06-15 00:12:31 +0500

    remove warnings and consoles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import React, { ReactElement, Suspense, lazy } from "react";
-import { Router, Routes, BrowserRouter, Route } from "react-router-dom";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
 import Layout from "../layout/layout";
-import history from "../utils/history";
 import LinearProgress from "@mui/material/LinearProgress";
 
 
@@ -10,8 +9,6 @@ const Location = lazy(() => import("../views/location/Location"));
 const Detail = lazy(() => import("../views/detail/Detail"));
 
 const IndexRouter: React.FC = (): ReactElement => {
-
-  console.log('Router')
   return (
 
     <BrowserRouter>
